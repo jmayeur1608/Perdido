c7d448d1b7340a17b45c97d8e7fd752e31fcd115
Ludovic Moncla <moncla.ludovic@gmail.com>
2016-04-08 17:32:26 +0200

    Added licence notice

--- POSprocessing/src/erig/postagger/Freeling.java
+++ POSprocessing/src/erig/postagger/Freeling.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2016 Ludovic Moncla <ludovic.moncla@univ-pau.fr>
+ * 
+ * This file is part of POSprocessing - Perdido project <http://erig.univ-pau.fr/PERDIDO/>
+ *
+ * POSprocessing is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * POSprocessing is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with POSprocessing.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+        	
 package erig.postagger;
 
 import java.io.BufferedReader;
