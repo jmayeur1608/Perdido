6aeb52ae086b984007d77d492871b245c59a2dc8
Ludovic Moncla <moncla.ludovic@gmail.com>
2016-04-14 11:13:43 +0200

    Added comments

--- LibTools/src/erig/tools/FileTools.java
+++ LibTools/src/erig/tools/FileTools.java
@@ -3,12 +3,12 @@
  * 
  * This file is part of LibTools - Perdido project <http://erig.univ-pau.fr/PERDIDO/>
  *
- * POSprocessing is free software: you can redistribute it and/or modify
+ * LibTools is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * POSprocessing is distributed in the hope that it will be useful,
+ * LibTools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
